61cecfe663e2d64d3ea07ddc689c5848e7986f83
wangchao <wangchaojs02@wangchaojs02demacbook-pro.local>
2015-10-26 21:49:51 +0800

    Initial Commit

---
+++ Memo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Memo
+//
+//  Created by wangchaojs02 on 15/10/26.
+//  Copyright © 2015年 wangchaojs02. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
